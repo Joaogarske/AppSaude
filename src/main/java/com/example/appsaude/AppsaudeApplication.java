0db6be0c1771c793d9af1eaf9db5faec7bdaa1d7
joao roberto <jrgarske5@gmail.com>
2024-11-24 16:23:48 -0300

    beck criado

---
+++ src/main/java/com/example/appsaude/AppsaudeApplication.java
@@ -0,0 +1,13 @@
+package com.example.appsaude;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AppsaudeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AppsaudeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/appsaude/AppsaudeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.appsaude;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AppsaudeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
